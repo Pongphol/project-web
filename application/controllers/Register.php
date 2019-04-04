e07c6ab76b8372784e3c19542b395b44ef2d1430
Yokk1e <satthabut.o@gmail.com>
2019-04-04 22:19:35 +0700

    edit header and create signUp

--- application/controllers/Register.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Register extends CI_Controller {
-
-        public function index()
-        {
-            $data['text'] = "test";
-            $this->load->view('v_register',$data);
-        }
-}
---
+++ application/modules/account/views/sign_up.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form>
+    <div class="form-group mb-3 my-5">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+</form>
+</body>
+</html>
